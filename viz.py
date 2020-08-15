7b2b3c4a5951bb6559b05cda2cb74011d276a15d
thamsuppp <isaactham2@hotmail.com>
2020-08-15 23:16:47 +0800

    Fixed the datatable bug -now fully working

--- viz.py
+++ viz.py
@@ -32,8 +32,13 @@ if 'DYNO' in os.environ:
 else:
     app_name = 'dash-scattermapboxplot'
 
-
+# Load the entire water data
 water_data = pd.read_csv('Water_FINAL.csv')
+
+# Round latitude and longitude to 3dp
+water_data['Latitude'] = water_data['Latitude'].apply(lambda x: round(x, 3))
+water_data['Longitude'] = water_data['Longitude'].apply(lambda x: round(x, 3))
+
 
 print('Loaded')
 
@@ -62,12 +67,12 @@ app.layout = html.Div([
 
     dcc.Dropdown(
         id = 'huc_dropdown',
+        # Give the dropdown values of HUCs in descending order of unique # of coordinates for that HUC
         options = [{'label': huc, 'value': huc} for huc in water_data.groupby('HUC12_')['coordinates'].nunique().sort_values(ascending = False).index.tolist()]
     ),
 
     dcc.Dropdown(
         id = 'year_dropdown',
-        # Get the HUC12s, sorted on the number of unique coordinates per HUC12
         options = [{'label': year, 'value': year} for year in water_data['Year'].unique()]
     ),
 
@@ -136,6 +141,9 @@ def update_station_list(click_data, datatable):
     print(click_data)
     longitude = click_data['points'][0]['lon']
     latitude = click_data['points'][0]['lat']
+
+    longitude = round(longitude, 3)
+    latitude = round(latitude, 3)
 
     station_info = water_data.loc[(water_data['Longitude'] == longitude) & (water_data['Latitude'] == latitude), 
                                 ['coordinates', 'HUC12_', 'HUCNAME_', 'COUNTY_', 'STATE_', 'Station', 'StationCode', 'Latitude', 'Longitude']]
@@ -274,127 +282,6 @@ def show_map(show_map_checklist_value, huc, parameter, year, month, aggregation)
             )
     }
 
-
-
-
-# # CALLBACK 1: Draws the map with layers based on the selected overlay variable, level (for multi-variables), 
-# # and transformation
-# @app.callback(
-#     Output("map", "figure"),
-#     [Input('df_store', 'data'),
-#      Input("layer_opacity_slider", "value"),
-#      Input("multi_variable_levels_dropdown", "value"),
-#      Input('layer_checkbox', 'values'),
-#      Input('transform_dropdown', 'value')],
-#      [State('overlay_variable_dropdown', 'value')])
-# def update_figure(df_store, layer_opacity_slider_value, multi_variable_levels_dropdown_value, layer_checkbox_value, 
-# transform_dropdown_value, variable_dropdown_value):
-
-#     #Load city data from df_store
-#     city_data = json.loads(df_store)
-
-#     #Create GDF with desired variable
-#     gdf = prepare_gdf(city_data, variable_dropdown_value, multi_variable_levels_dropdown_value)
-
-#     #Read what transformation the user is requesting
-#     if transform_dropdown_value != 'Value':
-#         if transform_dropdown_value == '% of Population':
-#             population_gdf = prepare_gdf(city_data, 'population', None)
-#             transformer_gdf = population_gdf.loc[:, ['zipcode', 'population']]
-#             transformer_gdf['population'] = transformer_gdf['population'] / 100
-#         elif transform_dropdown_value == 'Density (per square mile)':
-#             land_area_gdf = prepare_gdf(city_data, 'land_area_in_sqmi', None)
-#             transformer_gdf = land_area_gdf.loc[:, ['zipcode', 'land_area_in_sqmi']]
-#         elif transform_dropdown_value == '% of Median Household Income':
-#             household_income_gdf = prepare_gdf(city_data, 'median_household_income', None)
-#             transformer_gdf = household_income_gdf.loc[:, ['zipcode', 'median_household_income']]
-#             transformer_gdf['median_household_income'] = transformer_gdf['median_household_income'] / 100
-
-#         transformer_gdf.columns = ['zipcode', 'transformer']
-
-#         #Merge two dataframes
-#         gdf = gdf.merge(transformer_gdf, on = 'zipcode', how = 'inner')
-#         gdf[variable_dropdown_value] = gdf[variable_dropdown_value] / gdf['transformer']
-
-#         #Drop transformer row from gdf
-#         gdf = gdf.drop('transformer', axis = 1)
-#         gdf = gdf.replace([np.inf, -np.inf], np.nan)
-#         gdf = gdf.dropna()
-
-#     #Get color map (based on variable selected in dropdown)
-#     colors = set_overlay_colors(gdf[variable_dropdown_value])
-
-#     #Set hover text (based on variable selected in dropdown)
-#     gdf['hover_text'] = 'Zipcode: ' + gdf['zipcode'] + '<br /> ' + variable_dropdown_value + ': ' + round(gdf[variable_dropdown_value], 1).astype(str)
-
-
-#     is_layer_visible = len(layer_checkbox_value) == 1
-
-#     #Layers: District polygons, each with different colors defined by color map
-#     layers=[{
-#         'name': 'Population',
-#         'sourcetype': 'geojson',
-#         'visible': is_layer_visible,
-#         'source': json.loads(gdf.loc[gdf.index == i, :].to_json()),
-#         'type': 'fill',   
-#         'color': colors[i],
-#         'opacity': layer_opacity_slider_value
-#         } for i in gdf.index]
-
-    
-#     data = [go.Scattermapbox(
-#             lat=gdf['LAT'], 
-#             lon=gdf['LON'], 
-#             mode='markers', 
-#             marker={'opacity': 0},
-#             text=gdf['hover_text'], 
-#             hoverinfo='text', 
-#             name= 'Philadelphia Districts')]
-
-#     return {"data": data,
-#             "layout": go.Layout(
-#                 autosize=True, 
-#                 hovermode='closest', 
-#                 showlegend=False, 
-#                 height=700,
-#                 mapbox={
-#                     'accesstoken': mapbox_access_token, 
-#                     'bearing': 0, 
-#                     'layers': layers,
-#                     'center': {'lat': gdf['LAT'][0], 'lon': gdf['LON'][0]}, 
-#                     'pitch': 0, 'zoom': 10,
-#                     "style": 'mapbox://styles/mapbox/streets-v10'
-#                     }
-#                 )
-#             }
-
-
-# #CALLBACK 2: For multi-variables, sets the dropdown options that allow user to choose which level to display the choropleth map for.
-# @app.callback(
-#     [Output("multi_variable_levels_dropdown", "options"),
-#      Output("multi_variable_levels_dropdown", "value"),
-#      Output("multi_variable_levels_dropdown", "style"),],
-#     [Input('df_store', 'data'),
-#     Input('overlay_variable_dropdown', 'value')])
-# def set_levels_dropdown_options(df_store, variable_dropdown_value):
-
-#     #Load city data
-#     city_data = json.loads(df_store)
-
-#     #Check whether single or multi variable
-#     single_var, multi_var = sort_variables_by_type(city_data)
-
-#     if variable_dropdown_value in multi_var:
-
-#         levels = get_multi_var_levels(city_data, variable_dropdown_value)
-
-#         options = [{'label': titlecase(e), 'value': e} for e in levels]
-#         value = options[0]['value']
-        
-#         return options, value, {'display': 'block'}
-    
-#     else:
-#         return [], [], {'display': 'none'}
 
 
 if __name__ == '__main__':
